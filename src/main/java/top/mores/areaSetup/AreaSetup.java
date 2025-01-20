fbb002f8f36aee6e4aafabd2d2d554365666e80e
Do0oMores <87219087+Do0oMores@users.noreply.github.com>
2025-01-20 23:43:40 +0800

    CREATE

---
+++ src/main/java/top/mores/areaSetup/AreaSetup.java
@@ -0,0 +1,17 @@
+package top.mores.areaSetup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AreaSetup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
