acee44d64d632e39642363afe19490c1025c0f9a
Do0oMores <87219087+Do0oMores@users.noreply.github.com>
2025-01-20 23:58:03 +0800

    :sparkles: 配置文件加载

--- src/main/java/top/mores/areaSetup/AreaSetup.java
+++ src/main/java/top/mores/areaSetup/AreaSetup.java
@@ -1,17 +1,37 @@
 package top.mores.areaSetup;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class AreaSetup extends JavaPlugin {
+    public static FileConfiguration config;
+    private static AreaSetup plugin;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+        loadConfig();
+        getLogger().info("Area Setup Enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Area Setup Disabled");
+    }
+
+    //加载配置文件
+    private void loadConfig() {
+        File cconfigFile = new File(getDataFolder(), "config.yml");
+        if (!cconfigFile.exists()) {
+            saveResource("config.yml", false);
+        }
+        config = getConfig();
+    }
+
+    //获取插件实例
+    public static AreaSetup getPlugin() {
+        return plugin;
     }
 }
